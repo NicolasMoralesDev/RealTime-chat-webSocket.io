411dbf38d56213d8b399c6e8a3c99bdcb0eeed6e
Nicolas <nicolasmoralesj03@gmail.com>
2022-12-05 00:10:16 -0300

    se sube primera version

---
+++ client/src/App.jsx
@@ -0,0 +1,68 @@
+import './App.css'
+import io from 'socket.io-client'
+import Form from 'react-bootstrap/Form';
+import { useEffect, useState } from 'react';
+
+const socket = io('http://localhost:4000');
+
+
+function App() {
+
+  const [mensage, setMensage] = useState('');
+  const [mensages, setMensages] = useState([{
+    body:'',
+    from:'',
+}]);
+
+
+  const recibirMensage = (e) => {
+
+    e.preventDefault()
+    socket.emit('message', mensage)
+
+    const newMessage = {
+      body: mensage,
+      from: "yo",
+    };
+    setMensages([ newMessage, ...mensages])
+    setMensage('')
+  };
+
+  useEffect(() => {
+
+    const receiveMessage = (mensage) => {
+      setMensages([mensage, ...mensages]);
+    };
+
+   socket.on("message", receiveMessage);
+
+    return()=>{
+    socket.off("message", receiveMessage);
+    
+    }
+
+  }, [mensages])
+  
+
+  return (
+    <div className="h-screen bg-zinc-800 text-white flex-items-center justify-center">
+    
+       <Form onSubmit={recibirMensage} className="bg-zinc-900 p-10">
+      <Form.Group className="mb-3" controlId="formBasicText">
+        <Form.Control className="border-2 bg-zinc-500 p-2 text-black w-full" type="text" value={mensage} onChange={e => setMensage(e.target.value)} placeholder="Escribe tu mensage..." />
+      </Form.Group>
+    </Form>
+ <ul className='h-80 overflow-y-auto'>
+ { mensages.map((element, index) => (
+      <li key={index} className={` table p-2 my-2 text-sm rounder-md ${mensage.from === "yo" ? "bg-sky-700 ml-auto": "bg-black"}` }> 
+        <p>{element.from} | {element.body}</p>
+      </li>
+      
+    ))}
+ </ul>
+   
+    </div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express  from "express";
+import morgan from "morgan";
+import { Server } from "socket.io";
+import http from 'http'
+import cors from 'cors'
+import path, { dirname, join } from 'path'
+import  { fileURLToPath } from 'url'
+import {PORT} from './config.js'
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const server = http.createServer(app);
+const io = new Server(server,  {
+     cors: {
+            origin: "*",
+  }
+});
+
+app.use(cors())
+app.use(morgan('dev'));
+
+io.on('connection', (socket) =>{
+    console.log("Nuevo usuario conectado, Su id es:",socket.id);
+    socket.on('message', (message) => {
+    socket.broadcast.emit('message',{
+      body: message.body,
+      from: socket.id
+    })
+    });
+})
+
+app.use(express.static(join(__dirname, '../client/build')));
+server.listen(PORT);
+
+console.log(`server started on port ${PORT}`);
